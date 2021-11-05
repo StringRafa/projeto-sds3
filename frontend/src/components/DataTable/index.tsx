7daf1105de3043005e95aa726e6d53ab705a2204
Rafael <rafa.jo.souza@hotmail.com>
2021-11-05 16:22:17 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,78 @@
+
+function DataTable() {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Ana Maria</td>
+                        <td>11</td>
+                        <td>8</td>
+                        <td>5017.00</td>
+                    </tr>
+                    <tr>
+                        <td>23/04/2021</td>
+                        <td>Paulo</td>
+                        <td>28</td>
+                        <td>20</td>
+                        <td>12017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Roberto</td>
+                        <td>42</td>
+                        <td>30</td>
+                        <td>20017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Stefanie</td>
+                        <td>36</td>
+                        <td>29</td>
+                        <td>19017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Rafael</td>
+                        <td>49</td>
+                        <td>45</td>
+                        <td>25017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Thomas</td>
+                        <td>45</td>
+                        <td>40</td>
+                        <td>24017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Andrei</td>
+                        <td>39</td>
+                        <td>35</td>
+                        <td>18017.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
