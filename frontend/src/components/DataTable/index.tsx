9f4a5c2c563dd64361b72fc52697978a4905b441
Rafael <rafa.jo.souza@hotmail.com>
2021-11-06 22:29:36 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,5 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
 
 function DataTable() {
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=1&size=10&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, []);
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -13,62 +34,17 @@ function DataTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Ana Maria</td>
-                        <td>11</td>
-                        <td>8</td>
-                        <td>5017.00</td>
-                    </tr>
-                    <tr>
-                        <td>23/04/2021</td>
-                        <td>Paulo</td>
-                        <td>28</td>
-                        <td>20</td>
-                        <td>12017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Roberto</td>
-                        <td>42</td>
-                        <td>30</td>
-                        <td>20017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Stefanie</td>
-                        <td>36</td>
-                        <td>29</td>
-                        <td>19017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Rafael</td>
-                        <td>49</td>
-                        <td>45</td>
-                        <td>25017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Thomas</td>
-                        <td>45</td>
-                        <td>40</td>
-                        <td>24017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Andrei</td>
-                        <td>39</td>
-                        <td>35</td>
-                        <td>18017.00</td>
-                    </tr>
+                    {page.content?.map(item => (
+                        <tr key={item.id}>
+                            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                            <td>{item.seller.name}</td>
+                            <td>{item.visited}</td>
+                            <td>{item.deals}</td>
+                            <td>{item.amount.toFixed(2)}</td>
+                        </tr>
+                    ))}
+
+
                 </tbody>
             </table>
         </div>
